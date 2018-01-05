4220e64625abe9e978be3d55ad9ae23d21eabbc8
Eureka2018 <eureka2018@126.com>
2018-01-05 10:00:21 +0800

    p4

--- code/pe-4.py
+++ code/pe-4.py
@@ -4,7 +4,15 @@ def is_palindrome(olds):
         if s[i] != s[-i-1]:
             return False
     return True
- 
+
+def is_palindrome_2(num):
+  r = 0
+  n = num
+  while n > 0:
+    r = r * 10 + n % 10
+    n = n / 10
+  return r == num
+
 
 def find_palindrome_1(r):
     mp = 0     
@@ -17,6 +25,22 @@ def find_palindrome_1(r):
                     # print i, j
     return mp 
 
+
+def find_palindrome_2(a, b):
+  mp = 0
+  i = b
+  while i >= a:
+    j = b
+    while j >= i:
+      p = i * j
+      if is_palindrome_2(p):
+        # print p, i, j
+        if mp < p:
+          mp = p
+      j -= 1
+    i -= 1
+  return mp
+
 r = range(100, 1000)
 mp = find_palindrome_1(r)
 print mp
@@ -25,3 +49,5 @@ r = range(316, 1000)
 mp = find_palindrome_1(r)
 print mp
 
+mp = find_palindrome_2(900, 999)
+print mp
