512aaf333c5961e607b63c7a7e4244b64927462d
Eureka2018 <eureka2018@126.com>
2018-01-10 17:04:36 +0800

    pe-13

--- code/pe-12.cpp
+++ code/pe-12.cpp
@@ -3,26 +3,30 @@
 #include <stdio.h>
 #include <cmath>
 
-uint64_t get_factor_number(uint64_t n) {
+int get_factor_number(int n) {
   //std::vector<int> factor;
-  uint64_t count = 1; //from 1
-  for (uint64_t k = 2; k <= n / 2 + 1; ++k)
+  int count = 0;
+  int m = (int)std::sqrt(n);
+  for (int k = 1; k <= m; ++k)
     if (n % k == 0)
-      ++count;
+      count += 2;
+
+  if (m * m == n) --count;
+
   return count;
 }
 
 
 int main(int argc, char* argv[]) {
-  uint64_t i = 3;
-  uint64_t n = 3;
-  uint64_t limit = 5;
+  int i = 3;
+  int n = 3;
+  int limit = 5;
 
   if (argc > 1) limit = std::atoi(argv[1]);
 
   while(get_factor_number(n) < limit)
     n += i++;
 
-  printf("%lu\n", n);
+  printf("%d\n", n);
   return 0;
 }
